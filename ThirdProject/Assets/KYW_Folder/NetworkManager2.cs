6496d63f9313a16ad7ecdd249f243b205521ee0c
zuhwan0205 <ljh20230379@gmail.com>
2025-06-14 15:27:45 +0900

    add_Scene

--- ThirdProject/Assets/KYW_Folder/NetworkManager2.cs
+++ ThirdProject/Assets/KYW_Folder/NetworkManager2.cs
@@ -1,85 +1,85 @@
-using UnityEngine;
-using Photon.Pun;
-using Photon.Realtime;
-using System.Collections.Generic;
-
-public class NetworkManager2 : MonoBehaviourPunCallbacks
-{
-    // 싱글톤 패턴 구현
-    public static NetworkManager2 Instance { get; private set; }
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    private void Start()
-    {
-        // 포톤 서버 연결
-        PhotonNetwork.ConnectUsingSettings();
-    }
-
-    // 서버 연결 성공 시 호출
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("서버 연결 성공");
-        PhotonNetwork.JoinLobby();
-    }
-
-    // 로비 입장 성공 시 호출
-    public override void OnJoinedLobby()
-    {
-        Debug.Log("로비 입장 성공");
-    }
-
-    // 방 생성 함수
-    public void CreateRoom(string roomName, int maxPlayers = 4)
-    {
-        RoomOptions options = new RoomOptions
-        {
-            MaxPlayers = (byte)maxPlayers,
-            IsVisible = true,
-            IsOpen = true
-        };
-
-        PhotonNetwork.CreateRoom(roomName, options);
-    }
-
-    // 방 참가 함수
-    public void JoinRoom(string roomName)
-    {
-        PhotonNetwork.JoinRoom(roomName);
-    }
-
-    // 방 퇴장 함수
-    public void LeaveRoom()
-    {
-        PhotonNetwork.LeaveRoom();
-    }
-
-    // 방 생성 성공 시 호출
-    public override void OnCreatedRoom()
-    {
-        Debug.Log("방 생성 성공");
-    }
-
-    // 방 참가 성공 시 호출
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("방 참가 성공");
-    }
-
-    // 방 퇴장 성공 시 호출
-    public override void OnLeftRoom()
-    {
-        Debug.Log("방 퇴장 성공");
-    }
-} 
+// using UnityEngine;
+// using Photon.Pun;
+// using Photon.Realtime;
+// using System.Collections.Generic;
+//
+// public class NetworkManager2 : MonoBehaviourPunCallbacks
+// {
+//     // 싱글톤 패턴 구현
+//     public static NetworkManager2 Instance { get; private set; }
+//
+//     private void Awake()
+//     {
+//         if (Instance == null)
+//         {
+//             Instance = this;
+//             DontDestroyOnLoad(gameObject);
+//         }
+//         else
+//         {
+//             Destroy(gameObject);
+//         }
+//     }
+//
+//     private void Start()
+//     {
+//         // 포톤 서버 연결
+//         PhotonNetwork.ConnectUsingSettings();
+//     }
+//
+//     // 서버 연결 성공 시 호출
+//     public override void OnConnectedToMaster()
+//     {
+//         Debug.Log("서버 연결 성공");
+//         PhotonNetwork.JoinLobby();
+//     }
+//
+//     // 로비 입장 성공 시 호출
+//     public override void OnJoinedLobby()
+//     {
+//         Debug.Log("로비 입장 성공");
+//     }
+//
+//     // 방 생성 함수
+//     public void CreateRoom(string roomName, int maxPlayers = 4)
+//     {
+//         RoomOptions options = new RoomOptions
+//         {
+//             MaxPlayers = (byte)maxPlayers,
+//             IsVisible = true,
+//             IsOpen = true
+//         };
+//
+//         PhotonNetwork.CreateRoom(roomName, options);
+//     }
+//
+//     // 방 참가 함수
+//     public void JoinRoom(string roomName)
+//     {
+//         PhotonNetwork.JoinRoom(roomName);
+//     }
+//
+//     // 방 퇴장 함수
+//     public void LeaveRoom()
+//     {
+//         PhotonNetwork.LeaveRoom();
+//     }
+//
+//     // 방 생성 성공 시 호출
+//     public override void OnCreatedRoom()
+//     {
+//         Debug.Log("방 생성 성공");
+//     }
+//
+//     // 방 참가 성공 시 호출
+//     public override void OnJoinedRoom()
+//     {
+//         Debug.Log("방 참가 성공");
+//     }
+//
+//     // 방 퇴장 성공 시 호출
+//     public override void OnLeftRoom()
+//     {
+//         Debug.Log("방 퇴장 성공");
+//     }
+// } 
--- ThirdProject/Assets/LJH_Folder/Script/UIManager.cs
+++ ThirdProject/Assets/LJH_Folder/Script/UIManager.cs
@@ -39,17 +39,17 @@ public class UIManager : MonoBehaviour
     
     private void SettingButton()
     {
-        SceneManager.LoadScene("JDHScene");
+        StartCoroutine(test1());
     }
     
     private void QuitButton()
     {
-        SceneManager.LoadScene("KKI_TestScene");
+        StartCoroutine(test2());
     }
 
     private void testButton()
     {
-        SceneManager.LoadScene("Inventory");
+        StartCoroutine(test3());
     }
     
     IEnumerator StartHostAndLoadLobby()
@@ -64,6 +64,81 @@ public class UIManager : MonoBehaviour
         {
             GameMode = GameMode.AutoHostOrClient,
             SessionName = "GameLobby",
+            Scene = scene,
+            SceneManager = runner.gameObject.GetComponent<NetworkSceneManagerDefault>() ?? runner.gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+        
+
+        yield return new WaitUntil(() => runner.IsRunning);
+
+        Debug.Log("[LoadingScene] Host started → MainScene 이동");
+
+        yield return new WaitForSeconds(1f);
+        //SceneManager.LoadScene("LobbyScene");
+    }
+    
+    IEnumerator test1()
+    {
+        Debug.Log("[LoadingScene] Starting as Host → Create + Join GlobalLobby.");
+        
+        int lobbySceneBuildIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/JDHScene.unity"); // 또는 직접 숫자
+        var scene = SceneRef.FromIndex(lobbySceneBuildIndex);
+        var sceneInfo = new NetworkSceneInfo();
+
+        var startGameTask = runner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.AutoHostOrClient,
+            SessionName = "test1",
+            Scene = scene,
+            SceneManager = runner.gameObject.GetComponent<NetworkSceneManagerDefault>() ?? runner.gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+        
+
+        yield return new WaitUntil(() => runner.IsRunning);
+
+        Debug.Log("[LoadingScene] Host started → MainScene 이동");
+
+        yield return new WaitForSeconds(1f);
+        //SceneManager.LoadScene("LobbyScene");
+    }
+    
+    IEnumerator test2()
+    {
+        Debug.Log("[LoadingScene] Starting as Host → Create + Join GlobalLobby.");
+        
+        int lobbySceneBuildIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/KKI_TestScene.unity"); // 또는 직접 숫자
+        var scene = SceneRef.FromIndex(lobbySceneBuildIndex);
+        var sceneInfo = new NetworkSceneInfo();
+
+        var startGameTask = runner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.AutoHostOrClient,
+            SessionName = "test2",
+            Scene = scene,
+            SceneManager = runner.gameObject.GetComponent<NetworkSceneManagerDefault>() ?? runner.gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+        
+
+        yield return new WaitUntil(() => runner.IsRunning);
+
+        Debug.Log("[LoadingScene] Host started → MainScene 이동");
+
+        yield return new WaitForSeconds(1f);
+        //SceneManager.LoadScene("LobbyScene");
+    }
+    
+    IEnumerator test3()
+    {
+        Debug.Log("[LoadingScene] Starting as Host → Create + Join GlobalLobby.");
+        
+        int lobbySceneBuildIndex = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/KYW_Inventory.unity"); // 또는 직접 숫자
+        var scene = SceneRef.FromIndex(lobbySceneBuildIndex);
+        var sceneInfo = new NetworkSceneInfo();
+
+        var startGameTask = runner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.AutoHostOrClient,
+            SessionName = "test3",
             Scene = scene,
             SceneManager = runner.gameObject.GetComponent<NetworkSceneManagerDefault>() ?? runner.gameObject.AddComponent<NetworkSceneManagerDefault>()
         });
