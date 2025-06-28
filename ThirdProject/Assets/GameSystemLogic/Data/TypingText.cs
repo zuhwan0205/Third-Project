e208b1f410798538c1fcef0426a8e3e8dda62f13
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2025-06-28 16:03:54 +0900

    [Feat] ScreenUIUpdate2

--- ThirdProject/Assets/GameSystemLogic/Data/TypingText.cs
+++ ThirdProject/Assets/GameSystemLogic/Data/TypingText.cs
@@ -25,4 +25,54 @@ public static class TypingText
 
         return seq;
     }
+    
+    public static void UpdateScoreTexts(TextMeshProUGUI yesScoreText, TextMeshProUGUI noScoreText, float yesChange, float noChange)
+    {
+        if (yesScoreText != null)
+        {
+            if (yesChange > 0)
+            {
+                yesScoreText.text = $"+{yesChange}";
+                yesScoreText.color = Color.green;
+            }
+            else if (yesChange < 0)
+            {
+                yesScoreText.text = $"{yesChange}";
+                yesScoreText.color = Color.red;
+            }
+            else
+            {
+                yesScoreText.text = "0";
+                yesScoreText.color = Color.white;
+            }
+        }
+        
+        if (noScoreText != null)
+        {
+            if (noChange > 0)
+            {
+                noScoreText.text = $"+{noChange}";
+                noScoreText.color = Color.green;
+            }
+            else if (noChange < 0)
+            {
+                noScoreText.text = $"{noChange}";
+                noScoreText.color = Color.red;
+            }
+            else
+            {
+                noScoreText.text = "0";
+                noScoreText.color = Color.white;
+            }
+        }
+    }
+    
+    public static void HideScoreTexts(TextMeshProUGUI yesScoreText, TextMeshProUGUI noScoreText)
+    {
+        if (yesScoreText != null)
+            yesScoreText.text = "";
+        
+        if (noScoreText != null)
+            noScoreText.text = "";
+    }
 }
